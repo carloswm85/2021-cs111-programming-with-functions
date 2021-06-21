02b3e401268ce163e4712277b875c2c20eb2f041
carloswm85 <carloswashingtonmercado@gmail.com>
2021-06-21 20:50:27 -0300

    w09 Finished

--- test-zone.py
+++ test-zone.py
@@ -1,3 +1,10 @@
-import random
+students = {
+    "10-450-1203" : "Jorge Soares",
+    "75-421-2310" : "Abdul Ali",
+    "07-103-5621" : "Michelle Davis"
+}
 
-random.
+name = students["75-421-2310"]
+print(name)
+
+print(len(students))
